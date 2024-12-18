d89c2ba19717feabfc569a6507d18c85ae05e9db
Sabareesh001 <hatrickda@gmail.com>
2024-12-18 13:10:12 +0530

    import fix

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,5 +1,5 @@
 import { useRoutes } from "react-router-dom";
-import Layout from "../pages/layout/Layout";
+import Layout from "../pages/layout/layout";
 import { privateRoutes, publicRoutes } from "./allRouter";
 import Theme from "../themes/theme";
 
